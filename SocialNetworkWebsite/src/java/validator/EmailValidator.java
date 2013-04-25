0eb529e255eb2e352e549a7f106147a8c1b07b1f
Xi Yang <Madfrog@192.168.1.136>
2013-04-24 23:39:02 -0400

    Validators

--- SocialNetworkWebsite/src/java/validator/EmailValidator.java
+++ SocialNetworkWebsite/src/java/validator/EmailValidator.java
@@ -12,6 +12,8 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.persistence.EntityManager;
+import server.EMF;
 
 /**
  *
@@ -23,9 +25,11 @@ public class EmailValidator implements Validator {
     private static final String EMAIL_PATTERN = "^([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)$";
     private Pattern pattern;
     private Matcher matcher;
+    private EntityManager em;
 
     public EmailValidator() {
         pattern = Pattern.compile(EMAIL_PATTERN);
+        em = EMF.createEntityManager();
     }
 
     @Override
@@ -34,6 +38,12 @@ public class EmailValidator implements Validator {
         matcher = pattern.matcher(value.toString());
         if (!matcher.matches()) {
             FacesMessage message = new FacesMessage("E-mail validation failed.", "Invalid E-mail format.");
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(message);
+        }
+
+        if (!em.createNamedQuery("User.findByEmail").setParameter("email", value.toString()).getResultList().isEmpty()) {
+            FacesMessage message = new FacesMessage("Email not avaiable", "This email has already been registered. Try another?");
             message.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(message);
         }
--- SocialNetworkWebsite/src/java/validator/UsernameValidator.java
+++ SocialNetworkWebsite/src/java/validator/UsernameValidator.java
@@ -12,6 +12,8 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.persistence.EntityManager;
+import server.EMF;
 
 /**
  *
@@ -23,9 +25,11 @@ public class UsernameValidator implements Validator {
     private static final String USERNAME_PATTERN = "^[[A-Za-z0-9]+[A-Za-z0-9,._-]*]{3,20}$";
     private Pattern pattern;
     private Matcher matcher;
+    private EntityManager em;
 
     public UsernameValidator() {
         pattern = Pattern.compile(USERNAME_PATTERN);
+        em = EMF.createEntityManager();
     }
 
     @Override
@@ -34,6 +38,12 @@ public class UsernameValidator implements Validator {
         matcher = pattern.matcher(value.toString());
         if (!matcher.matches()) {
             FacesMessage message = new FacesMessage("Username validation failed.", "Contains 3-20 alphanumeric, dashes and underscores without spaces.");
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(message);
+        }
+        
+        if (!em.createNamedQuery("User.findByScreenName").setParameter("screenName", value.toString()).getResultList().isEmpty()) {
+            FacesMessage message = new FacesMessage("Username not avaiable", "Someone already has that username. Try another?");
             message.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(message);
         }
